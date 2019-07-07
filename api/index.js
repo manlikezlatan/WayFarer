fd1e39e9ff7a9efd891dc4dcf1dfe5e8808e8cfb
Codack <codacking@gmail.com>
2019-07-07 13:06:20 +0100

    install body parser

--- api/index.js
+++ api/index.js
@@ -4,10 +4,10 @@ const app = express();
 
 const PORT = 7000;
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     return res.send ('The API is necessary to run this application');
 })
 
-app.listen(PORT, function () {
-    console.log('Server is running in port ' + PORT);
+app.listen(PORT, () => {
+    console.log(`Server is running in port ${PORT} `);
 });
